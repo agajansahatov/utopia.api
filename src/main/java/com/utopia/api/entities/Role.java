5137627241803e2780744528615207447e326ffc
Agajan Sahatov <agajan.st@gmail.com>
2024-05-15 05:11:37 +0800

    Build Roles entity

---
+++ src/main/java/com/utopia/api/entities/Role.java
@@ -0,0 +1,30 @@
+package com.utopia.api.entities;
+
+public class Role {
+    private Short id;
+    private String name;
+
+    public Role() {
+    }
+
+    public Role(Short id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Short getId() {
+        return id;
+    }
+
+    public void setId(Short id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
