eadd8f2dfd1fb641380438c110b31cc75415f124
Agajan Sahatov <agajan.st@gmail.com>
2024-06-15 16:24:47 +0800

    Update findProductDetailsById method of ProductRepository

--- src/main/java/io/github/agajansahatov/utopia/api/repositories/ProductRepository.java
+++ src/main/java/io/github/agajansahatov/utopia/api/repositories/ProductRepository.java
@@ -9,7 +9,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 
 @Repository
