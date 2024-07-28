b7d70da400b04778d135528619a9907cf979162a
Agajan Sahatov <agajan.st@gmail.com>
2024-07-28 17:37:49 +0800

    Move JwtToken Generation logic to the UserService and remove JwtTokenService class, and Controller unit tests. Currently, only have the integration tests for controllers and unit tests for services. Because that is efficient to test the application.

--- src/main/java/io/github/agajansahatov/utopia/api/controllers/AuthController.java
+++ src/main/java/io/github/agajansahatov/utopia/api/controllers/AuthController.java
@@ -1,14 +1,11 @@
 package io.github.agajansahatov.utopia.api.controllers;
 
 import io.github.agajansahatov.utopia.api.models.requestDTOs.AuthRequest;
-import io.github.agajansahatov.utopia.api.services.JwtTokenService;
+import io.github.agajansahatov.utopia.api.services.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,12 +18,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
     public static final String ENDPOINT_PATH = "/api/auth";
-    private final JwtTokenService tokenService;
+    private final UserService userService;
 
     @PostMapping
     public ResponseEntity<?> authenticate(@Valid @RequestBody AuthRequest authRequest) {
         try {
-            return ResponseEntity.ok(tokenService.generateToken(authRequest));
+            return ResponseEntity.ok(userService.generateToken(authRequest.getContact(), authRequest.getPassword()));
         } catch (AuthenticationException e) {
             throw new BadCredentialsException("Invalid credentials");
         }
--- src/main/java/io/github/agajansahatov/utopia/api/services/UserService.java
+++ src/main/java/io/github/agajansahatov/utopia/api/services/UserService.java
@@ -1,19 +1,33 @@
     package io.github.agajansahatov.utopia.api.services;
 
     import io.github.agajansahatov.utopia.api.entities.User;
+    import io.github.agajansahatov.utopia.api.models.requestDTOs.AuthRequest;
     import io.github.agajansahatov.utopia.api.repositories.UserRepository;
     import lombok.RequiredArgsConstructor;
+    import org.springframework.context.ApplicationContext;
+    import org.springframework.context.annotation.Lazy;
+    import org.springframework.security.authentication.AuthenticationManager;
+    import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+    import org.springframework.security.core.Authentication;
     import org.springframework.security.core.userdetails.UserDetails;
     import org.springframework.security.core.userdetails.UserDetailsService;
     import org.springframework.security.core.userdetails.UsernameNotFoundException;
     import org.springframework.security.crypto.password.PasswordEncoder;
+    import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+    import org.springframework.security.oauth2.jwt.JwtEncoder;
+    import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
     import org.springframework.stereotype.Service;
+
+    import java.time.Instant;
+    import java.time.temporal.ChronoUnit;
 
     @Service
     @RequiredArgsConstructor
     public class UserService implements UserDetailsService {
         private final UserRepository userRepository;
         private final PasswordEncoder passwordEncoder;
+        private final JwtEncoder jwtEncoder;
+        private final ApplicationContext applicationContext;
 
         // This method is a method of UserDetailsService,
         // which is used by the SecurityConfig
@@ -35,4 +49,40 @@
         public boolean userExists(String contact) {
             return userRepository.findByContact(contact).isPresent();
         }
+
+        public String generateToken(String userContact, String userPassword, boolean isTest) {
+            AuthenticationManager authenticationManager = applicationContext.getBean(AuthenticationManager.class);
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(userContact, userPassword)
+            );
+
+            Instant now = Instant.now();
+
+            // default value is false, which is set in the overloaded method
+            if(isTest) {
+                now = now.minusSeconds(5L);
+            }
+
+            // Assuming that getAuthorities() returns a collection with exactly one element (role)
+            String role = authentication.getAuthorities().iterator().next().getAuthority();
+
+            User user = (User) authentication.getPrincipal();
+            String fullName = user.getFirstname() + " " + user.getLastname();
+
+            JwtClaimsSet claims = JwtClaimsSet.builder()
+                    .issuer("utopia.api")
+                    .issuedAt(now)
+                    .expiresAt(now.plus(10, ChronoUnit.DAYS))
+                    .claim("id", user.getId())
+                    .subject(fullName)
+                    .claim("role", role)
+                    .build();
+
+            return this.jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+        }
+
+        public String generateToken(String userContact, String userPassword) {
+            return this.generateToken(userContact, userPassword, false);
+        }
+
     }
