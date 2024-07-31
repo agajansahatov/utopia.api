d85b1b4bb10b6e9ead54e140125b2c7c48fa975f
Agajan Sahatov <agajan.st@gmail.com>
2024-07-31 15:01:35 +0800

    Fix a bug in Test DatabaseCleanup

--- src/test/java/io/github/agajansahatov/utopia/api/config/TestDatabaseCleanup.java
+++ src/test/java/io/github/agajansahatov/utopia/api/config/TestDatabaseCleanup.java
@@ -1,6 +1,5 @@
 package io.github.agajansahatov.utopia.api.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.event.annotation.AfterTestClass;
@@ -8,22 +7,24 @@ import org.springframework.transaction.annotation.Transactional;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
-
+import jakarta.persistence.Query;
 import java.util.List;
 
 @Configuration
 @Profile("test")
-public class DatabaseCleanup {
+public class TestDatabaseCleanup {
 
     @PersistenceContext
     private EntityManager entityManager;
 
     @Transactional
     @AfterTestClass
+    @SuppressWarnings("unchecked")
     public void cleanupDatabase() {
-        List<String> tables = entityManager.createNativeQuery(
+        Query query = entityManager.createNativeQuery(
                 "SELECT table_name FROM information_schema.tables WHERE table_type = 'BASE TABLE' AND table_schema = 'utopia_test'"
-        ).getResultList();
+        );
+        List<String> tables = query.getResultList();
 
         entityManager.createNativeQuery("SET FOREIGN_KEY_CHECKS = 0").executeUpdate();
         for (String table : tables) {
